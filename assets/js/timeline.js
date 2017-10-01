a57600ae960361221d8584588e9dc01964e91efc
jasondreams0513 <jasondreams0513@gmail.com>
2017-09-30 23:12:05 -0500

    Added overfolow function for smaller date segments, changed font in buttons to grey default and white on focus, changed lines in timeline to teal for better readability of white font, added space between time out input and load button

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -23,11 +23,15 @@ Configuration for vis timeline.
  */
 var options = {
   autoResize: true,
-  width: '100%',
+  width: '91%',
   height: '185px',
   editable: false,
   moveable: true,
-  zoomable: true
+  zoomable: true,
+  min: new Date(2017, 0, 1),             //Lower limit of visible range
+  max: new Date(2018, 0, 1),             //Upper limit of visible range
+  zoomMin: 1000 * 60 * 60 * 24,           //One day in milliseconds
+  zoomMax: 1000 * 60 * 60 * 24 * 31 * 3  //Around 3 months in milliseconds
 };
 
 /*
