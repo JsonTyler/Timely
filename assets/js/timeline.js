2202e3a1837ed494533e499a3cb998bd33294533
JsonTyler <jasondreams0513@gmail.com>
2020-04-26 19:55:42 -0500

    Updates the UI/UX and color scheme.

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -22,16 +22,17 @@ var container = document.getElementById('visualization');
 Configuration for vis timeline.
  */
 var options = {
-  autoResize: true,
+  verticalScroll: true,
+  resizable: true,
   width: '100%',
-  height: '185px',
+  height: '400px',
   editable: false,
   moveable: true,
   zoomable: true,
   min: new Date(1900, 1, 1),             //Lower limit of visible range
   max: new Date(3000, 1, 1),             //Upper limit of visible range
   zoomMin: 1000 * 60 * 60 * 24,           //One day in milliseconds
-  zoomMax: 1000 * 60 * 60 * 24 * 31 * 3  //Around 3 months in milliseconds
+  zoomMax: 1000 * 60 * 60 * 24 * 31 * 60  //Around 36 months in milliseconds
 };
 
 /*
